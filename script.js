c40bd3fe8a9e3801933a24cc988c514bc330941f
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-14 08:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  let buttons = document.querySelectorAll('.btn');
+  let stopButton = document.querySelector('.stop');
+  let audio = new Audio();
+  let isPlaying = false;
+
+  buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      let soundFile = button.textContent.trim() + '.mp3';
+      audio.src = 'sounds/' + soundFile;
+      audio.play();
+      isPlaying = true;
+    });
+  });
+
+  stopButton.addEventListener('click', function() {
+    if (isPlaying) {
+      audio.pause();
+      audio.currentTime = 0;
+      isPlaying = false;
+    }
+  });
+});
